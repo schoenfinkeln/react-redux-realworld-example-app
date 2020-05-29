21fc005ae0649b2c6b8034a17f0c332b165c8b6b
Matthias Moosburger <matthias.moosburger@maibornwolff.de>
2020-05-29 08:46:13 +0200

    fix(agent): set create react env variable prefix

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = process.env.API_ROOT;
+const API_ROOT = process.env.REACT_APP_API_ROOT;
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
